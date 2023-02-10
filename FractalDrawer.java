0e33342d0f0ee672ab495eb8b444e91f59d5859b
uek-1 <vickey2183@gmail.com>
2023-02-09 20:52:43 -0600

    rectangle fractal, color

--- FractalDrawer.java
+++ FractalDrawer.java
@@ -22,7 +22,7 @@ public class FractalDrawer {
             drawTriangleFractal(200,200,0,200, Color.green, canvas, 0); //canvas is 800x800
         }
         else if (type.equals("rectangle")){
-            drawRectangleFractal(100,100,0,0,Color.decode("red"),canvas,0);
+            drawRectangleFractal(80,80,360,360,Color.red,canvas,0);
         }
         else{
             System.out.println("Invalid type! Try either circle, triangle, or rectangle.");
@@ -68,13 +68,36 @@ public class FractalDrawer {
     // TODO:
     // drawCircleFractal draws a circle fractal using recursive techniques
     public void drawCircleFractal(double radius, double x, double y, Color c, Canvas can, int level) {
+
     }
 
 
     //TODO:
     // drawRectangleFractal draws a rectangle fractal using recursive techniques
     public void drawRectangleFractal(double width, double height, double x, double y, Color c, Canvas can, int level) {
-    }
+            if (level <= 7) {
+                if (level %2 == 1){
+                    c = Color.black;
+                }
+                else {
+                    c = Color.blue;
+                }
+
+                Rectangle rect = new Rectangle(x, y, width, height);
+                rect.setColor(c);
+                can.drawShape(rect);
+
+                drawRectangleFractal(width * 0.8, height * 0.8, x + width, y + height, c, can, level + 1);
+                drawRectangleFractal(width * 0.8, height * 0.8, x - width*0.8, y + height, c, can, level + 1);
+                drawRectangleFractal(width * 0.8, height * 0.8, x + width, y - height, c, can, level + 1);
+                drawRectangleFractal(width * 0.8, height * 0.8, x - width*0.8, y - height, c, can, level + 1);
+            }
+            else {
+                System.out.println("Exceeded recursion amount");
+                return;
+            }
+            }
+
 
     //TODO:
     // main should ask user for shape input, and then draw the corresponding fractal.
