dd35cf36a56104eae3699e367e4bb9d34bc04f84
uek-1 <vickey2183@gmail.com>
2023-02-16 00:39:11 -0600

    Finished all fractals, colors, and area. Need to comment then submit.

--- FractalDrawer.java
+++ FractalDrawer.java
@@ -1,5 +1,6 @@
 // FractalDrawer class draws a fractal of a shape indicated by user input
 import java.awt.Color;
+import java.util.Random;
 import java.util.Scanner;
 
 public class FractalDrawer {
@@ -16,18 +17,21 @@ public class FractalDrawer {
         Canvas canvas = new Canvas();
         System.out.println("type" + type);
         if (type.equals("circle")) {
-            drawCircleFractal(100,0,0,Color.decode("red"),canvas,0);
+            totalArea = 0;
+            drawCircleFractal(100,350,350,Color.red,canvas,0);
         }
         else if (type.equals("triangle")){
-            drawTriangleFractal(200,200,0,200, Color.green, canvas, 0); //canvas is 800x800
+            totalArea = 0;
+            drawTriangleFractal(200,200,300,300, Color.green, canvas, 0); //canvas is 800x800
         }
         else if (type.equals("rectangle")){
-            drawRectangleFractal(80,80,360,360,Color.red,canvas,0);
+            totalArea = 0;
+            drawRectangleFractal(200,200,300,300,Color.red,canvas,0);
         }
         else{
             System.out.println("Invalid type! Try either circle, triangle, or rectangle.");
         }
-        return 0; // need to return area!
+        return totalArea; // need to return area!
     }
 
 
@@ -36,39 +40,42 @@ public class FractalDrawer {
     public void drawTriangleFractal(double width, double height, double x, double y, Color color, Canvas can, int level){ // ideally, returns double area of the fractal.
         if (level <= 7) {
             double scaleFactor = 1;
-            if (level % 2 == 1){
-                color = Color.red;
-            }
-            else {
-                color = Color.green;
-            }
-            System.out.println(level + ":" + scaleFactor +" w:" + width);
-            Triangle shapeTriangle1 = new Triangle(x, 200, width, height * Math.pow(-1, level));
-            shapeTriangle1.setColor(color);
+            Random random = new Random();
+            color = new Color(random.nextInt(255), random.nextInt(255), random.nextInt(255));
 
-            Triangle shapeTriangle2 = new Triangle(0, y, width, height);
-            shapeTriangle2.setColor(color);
+            Triangle triangle = new Triangle(x, y, width, height);
+            triangle.setColor(color);
 
-            Triangle shapeTriangle3 = new Triangle(x, y, width, height * Math.pow(-1, level));
-            shapeTriangle3.setColor(color);
 
-            can.drawShape(shapeTriangle1);
-            can.drawShape(shapeTriangle2);
-            can.drawShape(shapeTriangle3);
-
-            drawTriangleFractal(0.775304 *width, 0.775304 * height, x + width, y + 0.775304*height, color, can, level+1);
+            drawTriangleFractal(width/2, height/2, x+width, y+height/2, color, can, level+1);
+            drawTriangleFractal(width/2, height/2, x-width/2, y+height/2, color, can, level+1);
+            drawTriangleFractal(width/2, height/2, x+width/4, y+height/2, color, can, level+1);
+            can.drawShape(triangle);
+            totalArea = totalArea + triangle.calculateArea();
         }
-        else {
-            System.out.println("Exceeded recursion amount");
-            return;
-        }
+
     }
 
 
     // TODO:
     // drawCircleFractal draws a circle fractal using recursive techniques
     public void drawCircleFractal(double radius, double x, double y, Color c, Canvas can, int level) {
+        if(level <= 7) {
+            Random random = new Random();
+            Color color = new Color(random.nextInt(255), random.nextInt(255), random.nextInt(255));
 
+            Circle circle = new Circle(x, y, radius);
+            circle.setColor(color);
+
+
+
+            drawCircleFractal(radius / 2, x, y - radius - radius / 2, color, can, level + 1); // above solve x + new radius + y= x + old radius => y = old radius - e
+            drawCircleFractal(radius / 2, x, y + radius + radius/2, color, can, level + 1);
+            drawCircleFractal(radius/2, x - radius - radius / 2, y, color, can, level + 1); // left
+            drawCircleFractal(radius/2, x + radius + radius/2, y, color, can, level + 1); // right
+            can.drawShape(circle);
+            totalArea = totalArea + circle.calculateArea();
+        }
     }
 
 
@@ -76,26 +83,20 @@ public class FractalDrawer {
     // drawRectangleFractal draws a rectangle fractal using recursive techniques
     public void drawRectangleFractal(double width, double height, double x, double y, Color c, Canvas can, int level) {
             if (level <= 7) {
-                if (level %2 == 1){
-                    c = Color.black;
-                }
-                else {
-                    c = Color.blue;
-                }
-
+                Random random = new Random();
+                Color color = new Color(random.nextInt(255), random.nextInt(255), random.nextInt(255));
                 Rectangle rect = new Rectangle(x, y, width, height);
-                rect.setColor(c);
-                can.drawShape(rect);
+                rect.setColor(color);
 
-                drawRectangleFractal(width * 0.8, height * 0.8, x + width, y + height, c, can, level + 1);
-                drawRectangleFractal(width * 0.8, height * 0.8, x - width*0.8, y + height, c, can, level + 1);
-                drawRectangleFractal(width * 0.8, height * 0.8, x + width, y - height, c, can, level + 1);
-                drawRectangleFractal(width * 0.8, height * 0.8, x - width*0.8, y - height, c, can, level + 1);
+
+                drawRectangleFractal(width/2, height/2, x + width, y + height, c, can, level + 1); //BR
+                drawRectangleFractal(width/2, height/2, x - width/2, y + height, c, can, level + 1); //UR
+                drawRectangleFractal(width/2, height/2, x + width, y-height/2, c, can, level + 1); //BL
+                drawRectangleFractal(width/2, height/2, x - width/2, y - height/2, c, can, level + 1); //UL
+                can.drawShape(rect);
+                totalArea = totalArea + rect.calculateArea();
             }
-            else {
-                System.out.println("Exceeded recursion amount");
-                return;
-            }
+
             }
 
 
@@ -107,7 +108,7 @@ public class FractalDrawer {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter a shape:");
         String inputShape = scanner.nextLine();
-        System.out.println(fd.drawFractal(inputShape));
+        System.out.println("The total area is " + fd.drawFractal(inputShape));
 
     }
 }
